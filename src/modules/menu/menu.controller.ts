41ab52109be279097aac106d724d398e3c42507e
callysavio <callistusanwara@gmail.com>
2026-03-11 17:46:38 +0100

    fixes express

--- src/modules/menu/menu.controller.ts
+++ src/modules/menu/menu.controller.ts
@@ -25,7 +25,7 @@ import { JwtAuthGuard } from "../../auth/guards/jwt-auth.guard";
 import { RolesGuard } from "../../auth/guards/roles.guard";
 import { Roles } from "../../common/decorators/roles.decorator";
 import { Role } from "../../common/enums/role.enum";
-
+import { Express } from "express";
 @Controller("api/menu")
 export class MenuController {
   constructor(private readonly menuService: MenuService) {}
