7c66a1cb859c2696588bd65d9a27dd9357b859ba
callysavio <callistusanwara@gmail.com>
2026-04-07 15:28:27 +0100

    orders service fixes

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -5,13 +5,15 @@ import { OrdersService } from "./orders.service";
 import { Order } from "./entities/order.entity";
 import { OrderItem } from "./entities/order-item.entity";
 import { MenuItem } from "../menu/entities/menu-item.entity";
+import { Variation } from "../menu/entities/variation.entity";
+import { Addon } from "../menu/entities/addon.entity";
 // import { WhatsAppNotificationService } from '../whatsapp/whatsapp-notification.service';
 import { QRCodeModule } from "../qrcode/qrcode.module";
 import { NotificationsModule } from "../notifications/notifications.module";
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([Order, OrderItem, MenuItem]),
+    SequelizeModule.forFeature([Order, OrderItem, MenuItem, Variation, Addon]),
     QRCodeModule,
     NotificationsModule,
   ],
